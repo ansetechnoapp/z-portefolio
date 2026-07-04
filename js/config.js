5e2f6b762e4e72d81fe1ebe500dc45cd2e9e100e
ansetechnoapp <anselmetechnoapp@gmail.com>
2026-07-04 15:22:56 +0000

    fix: point portfolio public to zodev api

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const PORTFOLIO_CONFIG = {
   // Base URL for the Portfolio API (Public endpoints)
-  API_URL: 'https://api.zoddev.site/api/portfolio/v1/public',
+  API_URL: 'https://api.zodev.live/api/portfolio/v1/public',
   
   // Your API Key here (Required for verification)
   API_TOKEN: 'zb_5zvnge0DI5cJKSXDB8gOCNNtxJHSAVjLtNEv_FROqTo',
